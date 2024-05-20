948e2f67abd196af558d31403ff40d4971b2ecc3
webwebweb5 <phiriyakornman@gmail.com>
2024-05-20 22:39:10 +0700

    add cors origin

--- src/main/java/com/backend/questify/QuestifyApplication.java
+++ src/main/java/com/backend/questify/QuestifyApplication.java
@@ -8,81 +8,94 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QuestifyApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(QuestifyApplication.class, args);
+        SpringApplication.run(QuestifyApplication.class, args);
 
-	}
+    }
 
-	@Bean
-	public CommandLineRunner init(UserService userService) {
-		return args -> {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+            }
+        };
+    }
 
+    @Bean
+    public CommandLineRunner init(UserService userService) {
+        return args -> {
 
-			User studentUser1 = User.builder()
-								   .userName("student1")
-								   .firstName("Student")
-								   .lastName("User")
-								   .displayName("Student User")
-								   .password("studentpassword")
-								   .email("student1@example.com")
-								   .role(Role.STUDENT)
-								   .build();
-			userService.createUser(studentUser1);
+            User studentUser1 = User.builder()
+                    .userName("student1")
+                    .firstName("Student")
+                    .lastName("User")
+                    .displayName("Student User")
+                    .password("studentpassword")
+                    .email("student1@example.com")
+                    .role(Role.STUDENT)
+                    .build();
+            userService.createUser(studentUser1);
 
-			User professorUser = User.builder()
-								   .userName("admin")
-								   .firstName("Admin")
-								   .lastName("User")
-								   .displayName("Admin User")
-								   .password("adminpassword")
-								   .email("admin@example.com")
-								   .role(Role.PROFESSOR)
-								   .build();
-			userService.createUser(professorUser);
+            User professorUser = User.builder()
+                    .userName("admin")
+                    .firstName("Admin")
+                    .lastName("User")
+                    .displayName("Admin User")
+                    .password("adminpassword")
+                    .email("admin@example.com")
+                    .role(Role.PROFESSOR)
+                    .build();
+            userService.createUser(professorUser);
 
-			User studentUser2 = User.builder()
-									.userName("student2")
-									.firstName("Student")
-									.lastName("User")
-									.displayName("Student User")
-									.password("studentpassword")
-									.email("student2@example.com")
-									.role(Role.STUDENT)
-									.build();
-			userService.createUser(studentUser2);
+            User studentUser2 = User.builder()
+                    .userName("student2")
+                    .firstName("Student")
+                    .lastName("User")
+                    .displayName("Student User")
+                    .password("studentpassword")
+                    .email("student2@example.com")
+                    .role(Role.STUDENT)
+                    .build();
+            userService.createUser(studentUser2);
 
-			User studentUser3 = User.builder()
-									.userName("student3")
-									.firstName("Student")
-									.lastName("User")
-									.displayName("Student User")
-									.password("studentpassword")
-									.email("student3@example.com")
-									.role(Role.STUDENT)
-									.build();
-			userService.createUser(studentUser3);
+            User studentUser3 = User.builder()
+                    .userName("student3")
+                    .firstName("Student")
+                    .lastName("User")
+                    .displayName("Student User")
+                    .password("studentpassword")
+                    .email("student3@example.com")
+                    .role(Role.STUDENT)
+                    .build();
+            userService.createUser(studentUser3);
 
-			User studentUser4 = User.builder()
-									.userName("student4")
-									.firstName("Student")
-									.lastName("User")
-									.displayName("Student User")
-									.password("studentpassword")
-									.email("student4@example.com")
-									.role(Role.STUDENT)
-									.build();
-			userService.createUser(studentUser4);
+            User studentUser4 = User.builder()
+                    .userName("student4")
+                    .firstName("Student")
+                    .lastName("User")
+                    .displayName("Student User")
+                    .password("studentpassword")
+                    .email("student4@example.com")
+                    .role(Role.STUDENT)
+                    .build();
+            userService.createUser(studentUser4);
 
 
 //			Classroom classroom1 = Classroom.builder()
 //					.professor(professorUser).
 //											.build();
-		};
-	}
+        };
+    }
 
 }
