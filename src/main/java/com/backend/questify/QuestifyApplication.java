876d235cc8b67a0071c918c99860866fc525bda7
sorawee <veevisorawee@gmail.com>
2024-05-29 14:38:36 +0700

    can run without getting entity manager error

--- src/main/java/com/backend/questify/QuestifyApplication.java
+++ src/main/java/com/backend/questify/QuestifyApplication.java
@@ -8,15 +8,21 @@ import com.backend.questify.Service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.backend.questify.Repository.*")
+//@EnableJpaRepositories(
+//        entityManagerFactoryRef = "entityManagerF"
+//        )
+@EntityScan(basePackages = "com.backend.questify.Entity")
 public class QuestifyApplication {
-
 
     public static void main(String[] args) {
 
--- src/main/java/com/backend/questify/Security/UserDetailsServiceImpl.java
+++ src/main/java/com/backend/questify/Security/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package com.backend.questify.Security;
 
 import com.backend.questify.Entity.User;
 import com.backend.questify.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,14 +16,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@Autowired
+//	public UserDetailsServiceImpl(UserRepository userRepository) {
+//		this.userRepository = userRepository;
+//	}
 
 	private final UserRepository userRepository;
 
-	@Autowired
 	public UserDetailsServiceImpl(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
+
 
 	@Override
 	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
